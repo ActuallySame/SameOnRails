434685b457a7498c1057ce04fffe94918042a379
Kanaad Parvate <kanaadp@gmail.com>
2015-12-02 20:53:23 -0800

    pushing back to old

--- app/models/following.rb
+++ app/models/following.rb
@@ -1,4 +1,5 @@
 class Following < ActiveRecord::Base
 	belongs_to :user_a, class_name: :User, foreign_key: :a_id
 	belongs_to :user_b, class_name: :User, foreign_key: :b_id
+        validates_uniqueness_of :user_a, :scope => [:user_b]
 end
